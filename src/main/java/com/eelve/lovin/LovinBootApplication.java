c1cd389096402fe84afec9ec0881d88bbd64a190
eelve <zzlve@foxmail.com>
2022-03-19 12:01:26 +0800

    初始化lovin-boot提交

---
+++ src/main/java/com/eelve/lovin/LovinBootApplication.java
@@ -0,0 +1,13 @@
+package com.eelve.lovin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovinBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovinBootApplication.class, args);
+    }
+
+}
